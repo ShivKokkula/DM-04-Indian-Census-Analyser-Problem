21cf52aa386ed29a1e86568978e06ff8fc196e9b
ShivKokkula <shivbk987@gmail.com>
2021-07-04 19:56:33 +0530

    Refactor - added support for List and Iterator

--- main/java/censusanalyser/ICSVBuilder.java
+++ main/java/censusanalyser/ICSVBuilder.java
@@ -2,8 +2,11 @@ package censusanalyser;
 
 import java.io.Reader;
 import java.util.Iterator;
+import java.util.List;
 
-public interface ICSVBuilder {
-    public<E> Iterator<E> getCSVFileIterator(Reader reader, Class csvClass)
+public interface ICSVBuilder<E> {
+    public Iterator<E> getCSVFileIterator(Reader reader, Class csvClass)
                                                 throws CSVBuilderException;
+    public List<E> getCSVFileList(Reader reader, Class csvClass)
+            throws CSVBuilderException;
 }
